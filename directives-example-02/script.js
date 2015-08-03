1254675b692745d1f609288174554f2bcb4c678d
Gabriel Tomé <gabrieltsistemas@hotmail.com>
2015-08-02 21:54:35 -0300

    directives - use ng-repeat

---
+++ directives-example-02/script.js
@@ -0,0 +1,33 @@
+// code is here
+var app = angular.module("app1", []);
+
+app.controller('controller1', function ($scope) {
+  $scope.users = myUsers;
+});
+
+var myUsers = [{
+    name: 'Goku',
+    type: 'Sayajin',
+    agility: 500,
+    power: 500
+},{
+    name: 'Gohan',
+    type: 'Sayajin',
+    agility: 100,
+    power: 100
+}, {
+    name: 'Yamcha',
+    type: 'Human',
+    agility: 10,
+    power: 10
+}, {
+    name: 'Piccolo',
+    type: 'Namekuseijin',
+    agility: 50,
+    power: 30
+}, {
+    name: 'Vegeta',
+    type: 'Sayajin',
+    agility: 90,
+    power: 100
+}];
